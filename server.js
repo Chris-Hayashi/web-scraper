ad88cc014a70f4aa1980c16b96a9baa5c52c5acf
Christopher Hayashi <Chrisrhayashi@gmail.com>
2020-02-10 15:08:17 -0800

    modified PORT

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = require("./models");
 //Initializing express
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //Parse request body as JSON
 app.use(express.urlencoded({ extended: true}));
