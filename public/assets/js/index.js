a2fb229c73972eede0f3be6bcfff5e525461fb75
Christopher Hayashi <Chrisrhayashi@gmail.com>
2020-02-12 13:56:30 -0800

    added modal to save notes

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,49 +1,22 @@
-// $.getJSON("/sports", function(data) {
-//     for (var i = 0; i < data.length; i++) {
-        
-//     }
-// })
-
-// console.log
-
-// $("#saved").on("click", function(event) {
-//     event.preventDefault();
-
-//     $.ajax({
-//         url: "/sports",
-//         // url: "/scrape",
-//         type: "GET"
-//     }).then(function(data) {
-//         // console.log("Response: " + JSON.stringify(data));
-//     });
-// })
-
 $(".saveArticle").on("click", function(event) {
     event.preventDefault();
-    console.log("saveArticle click event is working");
-    console.log("This.val(): " + $(this).val());
-    // $.getJSON("/sports/" + $(this).val().split("=")[1], function(data) {
+    // console.log("saveArticle click event is working");
+    // console.log("This.val(): " + $(this).val());
 
-    // })
     $.ajax({
         url: "/api/sports/" + $(this).val(),
-        // url: "/scrape",
         type: "PUT"
     }).then(function(data) {
-        console.log("Response: " + JSON.stringify(data));
+        // console.log("Response: " + JSON.stringify(data));
         location.reload();
     });
 });
 
 $("#scrape").on("click", function() {
-    // event.preventDefault();
-    console.log("scrape event listener is working correctly.");
-    
     $.ajax({
         url: "/scrape", 
         type: "GET"
     }).then(function() {
-        console.log("ajax funtion is working correctly");
         location.reload();
     });
 })
@@ -54,7 +27,10 @@ $("#clear").on("click", function() {
         url: "/api/clear", 
         type: "GET"
     }).then(function() {
-        console.log("api/clear route call working correctly");
         location.reload();
     });
 })
+
+$("#saveNote").on("click", function() {
+    
+});
--- server.js
+++ server.js
@@ -26,21 +26,16 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //Connect to mongoose
-const MONGODB_URI = process.env.MONGODB_URI || 
-                  "mongodb://localhost/mongoHeadlines";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
 
 //Set Handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-//Array to contain scraped objects
-// var scraped = [];
 
 //ROUTES
 app.get("/", function(req, res) {
-    // console.log("/ route is called");
-    // res.render("index");
     db.Sport.find({})
         .then(function(dbSport) {
             // console.log("Value of dbSport: " + dbSport);
@@ -55,13 +50,8 @@ app.get("/scrape", function(req, res) {
     axios.get("http://espn.com").then(function(response) {
         var result = {};
 
-        console.log("axios is working");
         // console.log("Here is axios' response: " + response.data);
         const $ = cheerio.load(response.data);
-        
-        // console.log("# of entries: " + db.Sport.find({"title": title}).limit(1).size());
-
-        // console.log("# of entries: " + util.inspect(db.Sport.find({"link": "http://www.espn.com/video/clip?id=28675818"}).limit(1).size()));
 
         $(".video-play-button").each(function(i, element) {
             let link = $(this).attr("data-popup-href");
@@ -73,18 +63,6 @@ app.get("/scrape", function(req, res) {
                 .children(".contentItem__title")
                 .text();
 
-            // console.log("original value of link: " + link);
-            // console.log("Title of that link: " + title);
-
-
-            // if ((title !== "") && (scraped.some(obj => obj.link !== String(link)) || !scraped.length)) {
-
-            
-                // scraped.push({
-                //     "link": link,
-                //     "title": title
-                // });
-                // console.log("# of entries: " + db.Sport.find({"title": title})._fields);
                 if ((title !== "") && (link !== undefined) &&
                     (db.Sport.find({"title": title})._fields === undefined)) {
                     if (link[0] === "h")
@@ -97,31 +75,26 @@ app.get("/scrape", function(req, res) {
 
                     db.Sport.create(result)
                         .then(function(dbSport) {
-                            // console.log("This is the sport added to db: " + dbSport);
                         })
                         .catch(function(err) {
                             console.log(err);
                     });
                 }
-            // }
+
         });
-        // console.log("Value of scraped array" + JSON.stringify(scraped));
         db.Sport.find({})
         .then(function(dbSport) {
-            // console.log("Value of dbSport: " + dbSport);
             res.render("index", {sports: dbSport});
         })
         .catch(function(err) {
             res.json(err);
         });
-        // res.render("index");
     });
 });
 
 app.get("/saved", function(req, res) {
     db.Sport.find({})
         .then(function(dbSport) {
-            // console.log("Value of dbSport: " + dbSport);
             res.render("saved", {sports: dbSport});
         })
         .catch(function(err) {
@@ -133,24 +106,16 @@ app.get("/api/clear", function(req, res) {
     db.Sport.remove({}, function(err) {
         if (err)
             console.log(err);
-        else
-            console.log("Articles cleared");
     });
-    // location.reload();
 });
 
 app.put("/api/sports/:id", function(req, res) {
-    console.log("api route called");
-    // console.log(("http://www.espn.com/video/clip?id=".concat(req.params.id)));
-    // db.Sport.findOneAndUpdate({ _id: "http://www.espn.com/video/clip?id=".concat(req.params.id)}, { saved: true }, function(error, edited) {
     db.Sport.findOneAndUpdate({ _id: req.params.id}, {$set: { saved: true }}, function(error, edited) {
         if (error) {
             console.log(error);
             res.send(error);
           }
           else {
-            // Otherwise, send the result of our update to the browser
-            // console.log(edited);
             res.send(edited);
           }
     });
